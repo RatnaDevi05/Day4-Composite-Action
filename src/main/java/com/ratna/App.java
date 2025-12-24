cdb60d25198af23e782751b5c3a67e079306f360
Ratna Devi <ratna.2541@xcelevate.org>
2025-12-24 22:32:27 +0530

    Composite Action

---
+++ src/main/java/com/ratna/App.java
@@ -0,0 +1,13 @@
+package com.ratna;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
